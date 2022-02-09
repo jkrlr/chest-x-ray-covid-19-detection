0fbfdd73f46546710532fc9330585573d3908724
Jitendra Kumar <jitendrar048@gmail.com>
2022-02-09 11:41:45 +0530

    Add Predict Result Page
    *It shows the result predicted by model of uploaded chest x-ray image
    *Remove url and console logs from upload image input ajax

--- predict/views.py
+++ predict/views.py
@@ -1,10 +1,9 @@
-import os
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
+from django.http import JsonResponse
 from django.conf import settings
 
-from .models import ChestXRayImage
 from .forms import ChestXRayImageForm
+from .models import ChestXRayImage
 
 
 def upload_image(request):
@@ -12,12 +11,18 @@ def upload_image(request):
     if request.is_ajax():
         if form.is_valid():
             form.save()
-            return JsonResponse({'message' : 'hell yeah'})
+            return JsonResponse({'message': 'Form is saved successfully!'})
+
     context = {
         "form": form,
     }
-
     return render(request, 'predict/upload_image.html', context)
 
-# def predicted_result(request):
-#     pass
+
+def predict_result(request):
+    # print(request.POST.dict().get('image'))
+    obj = ChestXRayImage.objects.latest('id')
+    img_url = settings.MEDIA_URL + str(obj.image)
+
+    context = {'img_url': img_url}
+    return render(request, 'predict/predict_result.html', context)
